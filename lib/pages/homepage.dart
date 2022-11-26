e163f438c09b6a4254701ac5207eb5de2c12bd91
Kathiravan Sekar <kathiravanrs@gmail.com>
2022-11-26 14:50:40 -0500

    Added tabs

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -21,6 +21,23 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     var homeAppBar = AppBar(
+      bottom: const TabBar(
+        labelColor: kShrineBrown900,
+        tabs: [
+          Tab(
+              icon: Icon(
+            Icons.home_filled,
+          )),
+          Tab(
+              icon: Icon(
+            Icons.search,
+          )),
+          Tab(
+              icon: Icon(
+            Icons.shopping_bag,
+          )),
+        ],
+      ),
       elevation: 0.0,
       titleSpacing: 0.0,
       title: const Center(
@@ -33,46 +50,68 @@ class _HomePageState extends State<HomePage> {
             semanticLabel: 'shopping cart',
           ),
           onPressed: () {
-            // logout();
             Navigator.pushNamed(context, '/cart');
           },
         ),
       ],
     );
 
-    return Scaffold(
-      drawer: const SideDrawer(),
-      appBar: homeAppBar,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
-        child: FutureBuilder(
-          future: FirebaseDatabase.instance.ref("products").get(),
-          builder: (BuildContext context, AsyncSnapshot<DataSnapshot> snapshot) {
-            Widget widget;
-            if (snapshot.connectionState == ConnectionState.done) {
-              var snaps = snapshot.data?.children;
-              products.clear();
-              for (DataSnapshot snap in snaps!) {
-                String key = snap.key!;
-                double price = double.parse(snap.child("price").value!.toString());
-                String desc = snap.child("desc").value!.toString();
-                final title = snap.child("title").value!.toString();
-                final image = snap.child("imageURL").value!.toString();
-                Product p = Product(
-                    id: key, title: title, description: desc, price: price, imageUrl: image);
-                products.add(p);
-              }
-              if (kDebugMode) {
-                print(products.toString());
-              }
-              widget = ProductGrid(products: products);
-            } else {
-              widget = const Center(
-                child: CircularProgressIndicator(),
-              );
+    // return DefaultTabController(length: length, child: child)
+
+    var home = Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+      child: FutureBuilder(
+        future: FirebaseDatabase.instance.ref("products").get(),
+        builder: (BuildContext context, AsyncSnapshot<DataSnapshot> snapshot) {
+          Widget widget;
+          if (snapshot.connectionState == ConnectionState.done) {
+            var snaps = snapshot.data?.children;
+            products.clear();
+            for (DataSnapshot snap in snaps!) {
+              final key = snap.key!;
+              final price = double.parse(snap.child("price").value!.toString());
+              final desc = snap.child("desc").value!.toString();
+              final title = snap.child("title").value!.toString();
+              final image = snap.child("imageURL").value!.toString();
+              Product p =
+                  Product(id: key, title: title, description: desc, price: price, imageUrl: image);
+              products.add(p);
             }
-            return widget;
-          },
+            if (kDebugMode) {
+              print(products.toString());
+            }
+            widget = ProductGrid(products: products);
+          } else {
+            widget = const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return widget;
+        },
+      ),
+    );
+
+    var search = const Padding(
+      padding: EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+      child: Center(child: Text("Search")),
+    );
+
+    var orders = const Padding(
+      padding: EdgeInsets.symmetric(horizontal: kDefaultPaddin),
+      child: Center(child: Text("Orders")),
+    );
+
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        drawer: const SideDrawer(),
+        appBar: homeAppBar,
+        body: TabBarView(
+          children: [
+            home,
+            search,
+            orders,
+          ],
         ),
       ),
     );
